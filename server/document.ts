672113ec721071f9f9ba420ef7a9b155e3455e7f
Aman jyoti Deka <amanjyoti.deka2020@vitstudent.ac.in>
2024-03-06 20:24:34 +0530

    optimize

--- server/document.ts
+++ server/document.ts
@@ -5,31 +5,24 @@ import temp from 'temp';
 import path from "path";
 import { replace, PatternDict } from './utils';
 
-import {writeJsonFile} from 'write-json-file';
-import {loadJsonFile} from 'load-json-file';
+import { writeJsonFile } from 'write-json-file';
+import { loadJsonFile } from 'load-json-file';
 
 import tex_file from './templates/simple/tex_file';
 import cls_file from './templates/simple/cls_file';
 
 
 
-
-
-
-export  async function create(templateFolder: string, obj: PatternDict): Promise<Buffer> {
+export async function create(templateFolder: string, obj: PatternDict): Promise<Buffer> {
 
     return new Promise((res, rej) => {
         temp.track();
-        fs.writeFileSync(path.join(process.cwd(), 'server', 'text.cls'), cls_file);
 
         temp.mkdir('temp', (err, dirPath) => {
-            const cls = fs.readFileSync(path.join(process.cwd(), 'server', templateFolder, 'resume.cls'), 'utf8');
-
-
             const doc = replace(tex_file, obj);
 
             fs.writeFileSync(path.join(dirPath, 'resume.tex'), doc);
-            fs.writeFileSync(path.join(dirPath, 'resume.cls'), cls);
+            fs.writeFileSync(path.join(dirPath, 'resume.cls'), cls_file);
 
 
             const ls = spawn("latexmk", ["-pdf"], { cwd: dirPath });
@@ -58,18 +51,20 @@ export  async function create(templateFolder: string, obj: PatternDict): Promise
 
 }
 
-export async function update_pattern_dict(obj:PatternDict){
+export async function update_pattern_dict(obj: PatternDict) {
     await writeJsonFile('data.json', obj);
 }
 
-export async function get_pattern_dict(){
-    return await loadJsonFile('data.json',{reviver:(key,value)=>{
-        return value as string;
-    }}) as PatternDict;
+export async function get_pattern_dict() {
+    return await loadJsonFile('data.json', {
+        reviver: (key, value) => {
+            return value as string;
+        }
+    }) as PatternDict;
 }
 
-export default async function  generate_document(){
+export default async function generate_document() {
     const obj = await get_pattern_dict();
     console.log(obj);
-    return create('templates',obj);
+    return create('templates', obj);
 }
